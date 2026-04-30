1f33624054e35d4f7b59adc449e4d564bf85f9a9
Tom Li <tomli1@utexas.edu>
2026-04-30 17:20:15 -0500

    Initial commit

---
+++ battle.py
@@ -0,0 +1,34 @@
+import random
+
+player = {
+    "name": "Pikachu",
+    "hp": 35,
+    "attack": 12
+}
+
+enemy = {
+    "name": "Charmander",
+    "hp": 39,
+    "attack": 10
+}
+
+
+def attack(attacker, defender):
+    damage = random.randint(5, attacker["attack"])
+    defender["hp"] -= damage
+    print(f'{attacker["name"]} attacks {defender["name"]} for {damage} damage!')
+
+
+def show_status():
+    print(f'{player["name"]}: {player["hp"]} HP')
+    print(f'{enemy["name"]}: {enemy["hp"]} HP')
+
+
+print("Battle Start!")
+show_status()
+
+attack(player, enemy)
+attack(enemy, player)
+
+print("\nAfter one round:")
+show_status()
